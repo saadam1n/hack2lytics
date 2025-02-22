b947402ffaeee78d8402d5a1968187c2292e9e77
Saad Amin <58188800+saadam1n@users.noreply.github.com>
2025-02-22 16:18:35 -0500

    Add thread functionality

--- inference/assistant.py
+++ inference/assistant.py
@@ -2,6 +2,7 @@
 
 """
 args:
+    session id: session id of the current thread (map from session id to thread)
     prompt: prompt given by user as string
     local_file_path: path to downloaded file as string on this machine if user provided file (otherwise it is None)
     request: network request as string if user provided file (otherwise it is None)
@@ -13,7 +14,13 @@ return value:
 """
 
 
-def generate_response(prompt, local_file_path, request):
+def generate_response(session_id, prompt, local_file_path, request):
     # replace pass with your own code
     print("Prompt:", prompt)
     return "hes"
+
+"""
+Creates a new thread id and returns the OpenAI thread id
+"""
+def create_thread_id():
+    pass
--- inference/flask_server.py
+++ inference/flask_server.py
@@ -2,15 +2,22 @@ from flask import Flask, request, Response, stream_with_context
 import time
 import os
 
-from assistant import generate_response
+from assistant import generate_response, create_thread_id
 
 app = Flask(__name__)
+
+@app.route("/create_thread", methods="POST")
+def create_thread():
+    id = create_thread_id()
+    return str(id), 200, {'Content-Type': 'text/plain'}
+
 
 @app.route('/message', methods=['POST'])
 def message():
     # Get JSON data from the request
     data = request.get_json()
-    user_prompt = data.get('prompt') if data else None
+    session_id = data.get('session') 
+    user_prompt = data.get('prompt') 
     user_file = data.get('file') if data else None
     user_request = data.get('request') if data else None
 
@@ -28,8 +35,7 @@ def message():
             f.write(user_file)
 
     # Return a streaming response using the generator function
-    response_text = generate_response(user_prompt, file_path, user_request)
-
+    response_text = generate_response(session_id, user_prompt, file_path, user_request)
 
     # wipe ass after taking a shit
     if user_file:
